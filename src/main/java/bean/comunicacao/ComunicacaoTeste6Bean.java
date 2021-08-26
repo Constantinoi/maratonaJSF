ad5d6aa2e67a2adc778f84d607ecf459dd8320aa
constantinoi <pescaalma@gmail.com>
2021-08-26 10:43:57 -0300

    Comunicacao PART 07,Passando atributos entre beans com FlashScope

--- src/main/java/bean/comunicacao/ComunicacaoTeste6Bean.java
+++ src/main/java/bean/comunicacao/ComunicacaoTeste6Bean.java
@@ -2,6 +2,8 @@ package bean.comunicacao;
 
 import model.Estudante;
 
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -16,9 +18,11 @@ public class ComunicacaoTeste6Bean implements Serializable {
 
     private List<Estudante> estudanteList = Estudante.estudanteList();
 
-    public String editar(int index){
-        Estudante estudante = estudanteList.get(index);+
-                return "";
+    public String editar(int index) {
+        Estudante estudante = estudanteList.get(index);
+        Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+        flash.put("estudante", estudante);
+        return "comunicacao7?faces-redirect=true";
     }
 
     public List<Estudante> getEstudanteList() {
@@ -28,4 +32,6 @@ public class ComunicacaoTeste6Bean implements Serializable {
     public void setEstudanteList(List<Estudante> estudanteList) {
         this.estudanteList = estudanteList;
     }
+
+
 }
--- src/main/java/bean/comunicacao/ComunicacaoTeste7Bean.java
+++ src/main/java/bean/comunicacao/ComunicacaoTeste7Bean.java
@@ -1,5 +1,9 @@
 package bean.comunicacao;
 
+import model.Estudante;
+
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -10,6 +14,18 @@ import java.io.Serializable;
 @Named
 @ViewScoped
 public class ComunicacaoTeste7Bean implements Serializable {
+    private Estudante estudante;
 
-    
+    public void init() {
+        Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+        estudante = (Estudante) flash.get("estudante");
+    }
+
+    public Estudante getEstudante() {
+        return estudante;
+    }
+
+    public void setEstudante(Estudante estudante) {
+        this.estudante = estudante;
+    }
 }
